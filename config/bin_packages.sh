54156c620dedd3efe95be4b015362b310e712da5
Peter Liveyns <peter.liveyns@gmail.com>
2023-11-06 20:00:22 +0100

    20231106 Debugging config/bin_packages.sh

--- config/bin_packages.sh
+++ config/bin_packages.sh
@@ -5,7 +5,7 @@
 
 echo "--- Installing binary packages from download link in recipe.yml --" && \
 
-binary_packages=$(yq '.binary[]' < /etc/toolbx-recipe.yml | sed -e "s/: /\&/") && \
+binary_packages=$(yq '.binary[]' < /tmp/config/recipe.yml | sed -e "s/: /\&/") && \
 for pkg in $binary_packages; do \
   bin=$(echo $pkg | cut -d'&' -f1 -); \
   url=$(echo $pkg | cut -d'&' -f2 -); \
