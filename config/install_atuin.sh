598499a9a085c7e3eb42aabb21f1b0e35a4c7469
Peter Liveyns <peter.liveyns@gmail.com>
2025-01-03 21:19:01 +0100

    20250103 Debug atuin install

--- config/install_atuin.sh
+++ config/install_atuin.sh
@@ -5,9 +5,11 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-#curl --proto '=https' --tlsv1.2 -LsSf https://github.com/atuinsh/atuin/releases/download/v18.4.0/atuin-installer.sh | sh
-
-#wget https://github.com/atuinsh/atuin/releases/download/v18.4.0/atuin-x86_64-unknown-linux-gnu.tar.gz
-
-curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+cd /tmp
+wget https://github.com/atuinsh/atuin/releases/download/v18.4.0/atuin-x86_64-unknown-linux-gnu.tar.gz
+tar xf atuin*.tar.gz
+mv atuin*/atuin /usr/local/bin/
+chmod +x /usr/local/bin/atuin
+rm atuin*.tar.gz
+rm -Rf atuin*
 
